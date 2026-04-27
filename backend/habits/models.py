8136f24ab11ae59c2d43da8bf5e7feb89256c288
Krzysztof Stasiak <krzsta@proton.me>
2026-04-27 21:27:47 +0200

    Add initial migration and models for Habit and HabitLog

--- backend/habits/models.py
+++ backend/habits/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Habit(models.Model):
+    class Type(models.TextChoices):
+        GOOD = 'good'
+        BAD = 'bad'
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    desc = models.CharField(max_length=200)
+    start_date = models.DateField()
+    type = models.CharField(max_length=10, choices=Type.choices)
+    
+    def __str__(self):
+        return self.title
+
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
+    log_date = models.DateField()
+    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['habit', 'log_date'], name='unique_log_for_habit')
+        ]
