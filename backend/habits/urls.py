55d38153d4f947bd22a6ff7aa954e80b93ee9c70
Krzysztof Stasiak <krzsta@proton.me>
2026-04-28 00:50:18 +0200

    Implement CRUD operations for Habit model in HabitView

--- backend/habits/urls.py
+++ backend/habits/urls.py
@@ -3,4 +3,5 @@ from .views import HabitView
 
 urlpatterns = [
     path('', HabitView.as_view()),
+    path('<int:pk>/', HabitView.as_view()),
 ]
--- backend/habits/views.py
+++ backend/habits/views.py
@@ -19,7 +19,32 @@ class HabitView(APIView):
     
     
     def get(self, request):
-        items = Habit.objects.filter(owner=request.user)
-        serializer = HabitSerializer(items, many=True)
+        habits = Habit.objects.filter(owner=request.user)
+        serializer = HabitSerializer(habits, many=True)
         return Response(serializer.data)
     
+    
+    def get_object(self, pk, user):
+        try:
+            return Habit.objects.get(pk=pk, owner=user)
+        except Habit.DoesNotExist:
+            return None
+        
+    
+    def put(self, request, pk):
+        habit = self.get_object(pk, request.user)
+        if not habit:
+            return Response({'message': 'Habit not found.'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = HabitSerializer(habit, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Habit updated.'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    def delete(self, request, pk):
+        habit = self.get_object(pk, request.user)
+        if not habit:
+            return Response({'message': 'Habit not found.'}, status=status.HTTP_404_NOT_FOUND)
+        habit.delete()
+        return Response({'message': 'Habit deleted.'}, status=status.HTTP_200_OK)
