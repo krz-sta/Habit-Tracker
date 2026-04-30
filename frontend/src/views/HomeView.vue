b74093900cddb2d685d60a7a716957090984c6bc
Krzysztof Stasiak <krzsta@proton.me>
2026-04-30 23:36:31 +0200

    Add: first basic homeview

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useHabitStore } from '@/stores/habits';
+import { useAuthStore } from '@/stores/auth';
+
+const habitStore = useHabitStore();
+const authStore = useAuthStore();
+
+onMounted(async () => {
+    await habitStore.fetchHabits();
+});
+
+</script>
+
 <template>
-    <div>Home</div>
+    <h1>Habit Tracker</h1>
+    <ul>
+        <li v-for="habit in habitStore.habits">{{ habit.title }}, {{ habit.start_date }}</li>
+    </ul>
+    <button @click="authStore.logout">Logout</button>
 </template>
