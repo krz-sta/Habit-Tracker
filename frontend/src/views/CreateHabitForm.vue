fbdfa50b2e871393685a769ee6b8e4651affb3ef
Krzysztof Stasiak <krzsta@proton.me>
2026-04-30 23:57:25 +0200

    Add: CreateHabitForm component for habit creation

---
+++ frontend/src/views/CreateHabitForm.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useHabitStore } from '@/stores/habits';
+
+const habitStore = useHabitStore();
+
+const title = ref('');
+const desc = ref('');
+const type = ref<'good' | 'bad' | null>(null);
+
+</script>
+
+<template>
+    <div>
+        <h2>Create a habit</h2>
+        <label>
+            Title:<input v-model="title" />
+        </label>
+        <label>
+            Description:<input v-model="desc" />
+        </label>
+        <h3>Type:</h3>
+        <label>
+            <input type="radio" value="good" v-model="type" /> A
+        </label>
+        <label>
+            <input type="radio" value="bad" v-model="type" /> B
+        </label>
+
+        <p v-if="type">Selected: {{ type }}</p>
+    </div>
+</template>
+<style scoped>
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,6 +3,8 @@ import { onMounted } from 'vue';
 import { useHabitStore } from '@/stores/habits';
 import { useAuthStore } from '@/stores/auth';
 
+import CreateHabitForm from './CreateHabitForm.vue';
+
 const habitStore = useHabitStore();
 const authStore = useAuthStore();
 
@@ -18,4 +20,5 @@ onMounted(async () => {
         <li v-for="habit in habitStore.habits">{{ habit.title }}, {{ habit.start_date }}</li>
     </ul>
     <button @click="authStore.logout">Logout</button>
+    <CreateHabitForm />
 </template>
