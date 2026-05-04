a12518863a398c96c03ba2b0f3a16d7d33e944ff
Krzysztof Stasiak <krzsta@proton.me>
2026-05-04 23:35:45 +0200

    Adjust JWT token lifetime.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,6 +140,6 @@ REST_FRAMEWORK = {
    }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
